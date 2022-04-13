0d62eb332c4471f7756cdcc5145107400a25c09f
wchaws <wch@amazon.com>
2022-04-13 10:29:59 +0000

    fix: construct test

--- source/constructs/test/ecs-image-handler.test.ts
+++ source/constructs/test/ecs-image-handler.test.ts
@@ -3,7 +3,11 @@ import { App } from '@aws-cdk/core';
 import { ECSImageHandlerStack } from '../lib/constructs-stack';
 
 test('Snapshot', () => {
-  const app = new App();
+  const app = new App({
+    context: {
+      buckets: ['bucket-0']
+    }
+  });
   const stack = new ECSImageHandlerStack(app, 'test');
 
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
